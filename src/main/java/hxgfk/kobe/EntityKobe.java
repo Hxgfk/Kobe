3bd0cb2d19ae57c0cb9b45c3ff92576895b97ebb
Hxgfk <b13924856129@qq.com>
2024-03-16 22:12:20 +0800

    1.0 version

--- src/main/java/hxgfk/kobe/EntityKobe.java
+++ src/main/java/hxgfk/kobe/EntityKobe.java
@@ -5,17 +5,16 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class EntityKobe extends EntityMob {
     public static final String entity_id = "entity_kobe";
@@ -30,10 +29,52 @@ public class EntityKobe extends EntityMob {
     protected void initEntityAI() {
         this.tasks.addTask(0, new EntityAISwimming(this));
         this.tasks.addTask(1, new EntityAIHurtByTarget(this, false));
-        this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.3D, true));
-        this.tasks.addTask(2, new EntityAIWatchClosest2(this, EntityPlayer.class, 4.5F, 1.0F));
-        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityLiving.class, 4F));
-        this.tasks.addTask(3, new EntityAIWanderAvoidWater(this, 1.1D));
+        this.tasks.addTask(1, new EntityAIAttackMelee(this, 0.96D, true));
+        this.tasks.addTask(1, new EntityAIRestrictOpenDoor(this));
+        this.tasks.addTask(2, new EntityAINearestMonsterAttack(this, 15));
+        this.tasks.addTask(2, new EntityAIOpenDoor(this, true));
+        this.tasks.addTask(3, new EntityAIWanderAvoidWater(this, 0.65D));
+        this.tasks.addTask(3, new EntityAIWander(this, 0.6D));
+        this.tasks.addTask(4, new EntityAIWatchClosest2(this, EntityPlayer.class, 4.5F, 5.0F));
+        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityLiving.class, 4F));
+    }
+
+    public static class EntityAINearestMonsterAttack extends EntityAIBase {
+        private final EntityCreature entity;
+        private final double attractRange;
+        private int maxTargets = 30;
+        private int attractedTargets = 0;
+
+        public EntityAINearestMonsterAttack(EntityCreature entity, double range) {
+            this.entity = entity;
+            this.attractRange = range;
+        }
+
+        @Override
+        public boolean shouldExecute() {
+            return true;
+        }
+
+        @Override
+        public void updateTask() {
+            List<EntityMob> nearbyMobs = this.entity.world.getEntitiesWithinAABB(EntityMob.class, this.entity.getEntityBoundingBox().grow(this.attractRange));
+
+            for (EntityMob mob : nearbyMobs) {
+                if (this.attractedTargets >= this.maxTargets) {
+                    break;
+                }
+
+                if (mob.getAttackTarget() == null) {
+                    mob.setAttackTarget(this.entity);
+                    this.attractedTargets++;
+                }
+            }
+        }
+
+        @Override
+        public void resetTask() {
+            this.attractedTargets = 0;
+        }
     }
 
     public static class model extends ModelPlayer {
